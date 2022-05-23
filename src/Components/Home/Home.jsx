458d268ef7d995996c87d1623cd03f3d8258cc8c
Adones Balica <adonesbalica@gmail.com>
2022-05-23 19:03:25 -0300

    Comonente de loading emplementado

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Feed from "../Feed/Feed";
+import Loading from "../Helper/Loading/Loading";
 
 function Home() {
   const location = useLocation();
